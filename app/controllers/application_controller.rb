6ba9b3d93af4f6c3a75f2fa007ad984e21cd6e20
Keith Isham <kisham8899@gmail.com>
2019-09-14 16:05:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,10 +16,15 @@ class ApplicationController < Sinatra::Base
     erb :signup
   end
 
-  post "/signup" do
-    #your code here
-
+post "/signup" do
+  user = User.new(:username => params[:username], :password => params[:password])
+ 
+  if user.save
+    redirect "/login"
+  else
+    redirect "/failure"
   end
+end
 
   get '/account' do
     @user = User.find(session[:user_id])
